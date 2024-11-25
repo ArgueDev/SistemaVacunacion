e1e58688fe47f09c7da96784e8490ce56c044fe8
ArgueDev <christianarguelloo97@gmail.com>
2024-11-25 17:53:33 -0500

    Ajustes en DB

--- src/database/DataBaseConnection.java
+++ src/database/DataBaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DataBaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/vacunas";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String USER = "root"; // Ingresar su usuario
+    private static final String PASSWORD = "root"; // Ingresar su password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
